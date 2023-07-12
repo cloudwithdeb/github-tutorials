f0dc7a9cf45efe493a57b297dbf4ab56f72fd25f
cloudwithdeb <cloudwithdeb@gmail.com>
2023-07-12 12:36:42 +0000

    testing github actions with azure OIDC

---
+++ vm2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo su
+apt update -y
+apt install apache2 -y
+systemctl enable apache2
+systemctl start apache2
+cd /var/www/html
+echo "<h1>Welcome to: $(hostname -f)</h1>" > index.html
+mkdir vm2
+echo "<h1>$(hostname -f)</h1>" > vm2/index.html
